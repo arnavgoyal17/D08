146ca5bb4a00a35798384ca02790111317ff294b
Arnav Goyal <arnavgoyal17@gmail.com>
2016-08-15 12:04:55 -0700

    Fixed Code

--- mimsmind0.py
+++ mimsmind0.py
@@ -24,77 +24,75 @@ numberTries = 0
 
 # function to check if the command line argument is valid or not, if valid assign the value to numberDigits
 # def CheckCommandLineArgument(argv):
-# 	if(len(argv) == 1):
-# 		return True
-# 	elif(len(argv) == 2):
-# 		# check if the 2nd input is actually an integer
-# 		try:
-# 			userLengthInput = int(argv[1])
-# 			if(userLengthInput <= 0):
-# 				raise ValueError
-# 			else:
-# 				numberDigits = userLengthInput
-# 			return True
-# 		except ValueError:
-# 			print("\n*********************************************")
-# 			print("Invalid User Input: " + str(argv[1]) + ". Please Enter A Valid Positive Integer")
-			# return False
+#   if(len(argv) == 1):
+#       return True
+#   elif(len(argv) == 2):
+#       # check if the 2nd input is actually an integer
+#       try:
+#           userLengthInput = int(argv[1])
+#           if(userLengthInput <= 0):
+#               raise ValueError
+#           else:
+#               numberDigits = userLengthInput
+#           return True
+#       except ValueError:
+#           print("\n*********************************************")
+#           print("Invalid User Input: " + str(argv[1]) + ". Please Enter A Valid Positive Integer")
+            # return False
 
 # function to start the game
 def StartGame(numberDigits):
-	# generate a random number based on the length input by the user
-	upperLimit = (10**numberDigits) - 1
-	mainNumber = random.randint(0, upperLimit)
-	# print(mainNumber)
+    # generate a random number based on the length input by the user
+    upperLimit = (10**numberDigits) - 1
+    mainNumber = random.randint(0, upperLimit)
+    # print(mainNumber)
 
-	print("\nLet's Play The mimsmind0 Game.\n")
+    print("\nLet's play the mimsmind0 game.\n")
 
-	StartGuessing(numberDigits, mainNumber, numberTries)
+    StartGuessing(numberDigits, mainNumber, numberTries)
 
 def StartGuessing(numberDigits, mainNumber, numberTries):
-	userInput = 0
-	# continuously ask for User Input and count the number of guesses it took to guess the right answer
-	while True:
-		try:
-			userInput = int(input("Enter A " + str(numberDigits) + " Digit Number: "))
-			if(len(str(userInput)) != numberDigits):
-				raise ValueError
-			numberTries += 1
+    userInput = 0
+    # continuously ask for User Input and count the number of guesses it took to guess the right answer
+    while True:
+        try:
+            userInput = int(input("Enter a " + str(numberDigits) + " digit number: "))
+            if(len(str(userInput)) != numberDigits):
+                raise ValueError
+            numberTries += 1
 
-			if(userInput == mainNumber):
-				print("Congratulations. You Have Guessed The Corrent Number In " + str(numberTries) + " Tries.")
-				break
-			elif(userInput < mainNumber):
-				print("Guess A Higher Number. Try Again!")
-			elif(userInput > mainNumber):
-				print("Guess A Lower Number. Try Again!")
-		except ValueError:
-			print("Invalid Input. Enter A Valid " + str(numberDigits) + " Integer.")
-			continue
+            if(userInput == mainNumber):
+                print("Congratulations. You guessed the correct number in " + str(numberTries) + " tries.")
+                break
+            elif(userInput < mainNumber):
+                print("Guess a higher number. Try again!")
+            elif(userInput > mainNumber):
+                print("Guess a lower number. Try again!")
+        except ValueError:
+            print("Invalid input. Enter a valid " + str(numberDigits) + " digit integer.")
+            continue
 
 def main():
-
-	numberDigits = 1 # number of digits
-	mainNumber = 0 # the corrent answer to the game
-	numberTries = 0
-	
-	# check user input
-	if(len(sys.argv) == 1):
-		StartGame(numberDigits)
-	elif(len(sys.argv) == 2):
-		# check if the 2nd input is actually an integer
-		try:
-			userLengthInput = int(sys.argv[1])
-			if(userLengthInput <= 0):
-				raise ValueError
-			else:
-				numberDigits = userLengthInput
-				StartGame(numberDigits, numberTries)
-			return True
-		except ValueError:
-			print("\n*********************************************")
-			print("Invalid User Input: " + str(sys.argv[1]) + ". Please Enter A Valid Positive Integer")
-			return False
+    numberDigits = 1 # number of digits
+    mainNumber = 0 # the corrent answer to the game
+    numberTries = 0
+    
+    # check user input
+    if(len(sys.argv) == 1):
+        StartGame(numberDigits)
+    elif(len(sys.argv) == 2):
+        # check if the 2nd input is actually an integer
+        try:
+            userLengthInput = int(sys.argv[1])
+            if(userLengthInput <= 0):
+                raise ValueError
+            else:
+                numberDigits = userLengthInput
+                StartGame(numberDigits, numberTries)
+            return True
+        except ValueError:
+            print("Invalid user input: " + str(sys.argv[1]) + ". Please enter a valid positive integer")
+            return False
 
 if __name__ == '__main__':
-	main()
+    main()
--- mimsmind1.py
+++ mimsmind1.py
@@ -39,108 +39,143 @@ numberTries = 0
 
 # function to start the game
 def StartGame(numberDigits):
-	# generate a random number based on the length input by the user
-	upperLimit = (10**numberDigits) - 1
-	mainNumber = random.randint(0, upperLimit)
-	mainNumber = str(mainNumber).zfill(numberDigits)
-	numberTries = (2**numberDigits) + numberDigits
+    # generate a random number based on the length input by the user
+    upperLimit = (10**numberDigits) - 1
+    mainNumber = random.randint(0, upperLimit)
+    mainNumber = str(mainNumber).zfill(numberDigits)
+    numberTries = (2**numberDigits) + numberDigits
+    # print(mainNumber)
 
-	print(mainNumber)
+    print("\nLet's play the mimsmind1 game. You have " + str(numberTries) + " guesses.\n")
 
-	print("\nLet's Play The mimsmind1 Game. You Have " + str(numberTries) + " Guesses.\n")
-
-	StartGuessing(numberDigits, mainNumber, numberTries)
+    StartGuessing(numberDigits, mainNumber, numberTries)
 
 def StartGuessing(numberDigits, mainNumber, numberTries):
-	userInput = ''
-	userInputInt = 0
-	userTries = 0
-	numberBulls = 0
-	numberCows = 0
-	validInput = False
-	firstInput = False
+    userInput = ''
+    userInputInt = 0
+    userTries = 0
+    numberBulls = 0
+    numberCows = 0
+    validInput = False
+    firstInput = False
 
-	while True:
-		if(userTries < numberTries):
-			try:
-				if(not firstInput):
-					userInput = input("Guess A " + str(numberDigits) + " Digit Number: ")
-					firstInput = True
-				else:
-					if(validInput):
-						userInput = input(str(numberBulls) + " bull(s), " + str(numberCows) + " cow(s). Try Again: ")
-						numberBulls = 0
-						numberCows = 0
-					else:
-						userInput = input("Invalid Number. Please Enter A " + str(numberDigits) + " Digit Number: ")
-						numberBulls = 0
-						numberCows = 0
-				
-				userInputInt = int(userInput)
-				if(len(userInput) != numberDigits):
-					raise ValueError
-				else:
-					userTries += 1
-					validInput = True
-			except ValueError:
-				validInput = False
-			finally:			
-			# continuously ask for User Input and count the number of guesses it took to guess the right answer
-				if(validInput):
-					numberBulls = 0
-					numberCows = 0
-					if(userInput == mainNumber):
-						print("Congratulations. You Have Guessed The Corrent Number In " + str(userTries) + " Tries.")
-						return
-					else:
-						numberBulls, numberCows = GetCowsBulls(userInput, mainNumber)
-		else:
-			print("Sorry. You Did Not Guess The Number In " + str(numberTries) + " Tries. The Correct Number Is " + str(mainNumber) + ".")
-			break
+    while True:
+        # check if the user has more tries left or not
+        if(userTries < numberTries):
+            try:
+                # different prompt for user's first input
+                if(not firstInput):
+                    userInput = input("Guess a " + str(numberDigits) + " digit number: ")
+                    firstInput = True
+                else:
+                    if(validInput):
+                        userInput = input(str(numberBulls) + " bull(s), " + str(numberCows) + " cow(s). Try again: ")
+                        numberBulls = 0
+                        numberCows = 0
+                    else:
+                        # userInput = input("Invalid number. Please enter a " + str(numberDigits) + " digit number: ")
+                        userInput = input("Invalid input. Try again: ")
+                        numberBulls = 0
+                        numberCows = 0
+                
+                userInputInt = int(userInput)
+                if(len(userInput) != numberDigits):
+                    raise ValueError
+                else:
+                    userTries += 1
+                    validInput = True
+            except ValueError:
+                validInput = False
+            finally:            
+            # continuously ask for User Input and count the number of guesses it took to guess the right answer
+                if(validInput):
+                    numberBulls = 0
+                    numberCows = 0
+                    if(userInput == mainNumber):
+                        print("Congratulations. You guessed the correct number in " + str(userTries) + " tries.")
+                        return
+                    else:
+                        numberBulls, numberCows = GetCowsBulls(userInput, mainNumber)
+        else:
+            print("Sorry. You did not guess the number in " + str(numberTries) + " tries. The correct number is " + str(mainNumber) + ".")
+            break
 
 def GetCowsBulls(userInput, mainNumber):
-	# this would return the number of bulls for the input given by the user
-	numberBulls = 0
-	numberCows = 0
+    # this would return the number of bulls for the input given by the user
+    numberBulls = 0
+    numberCows = 0
 
-	for n in range(len(str(userInput))):
-		if(str(mainNumber)[n] == str(userInput)[n]):
-			numberBulls += 1
-		else:
-			numberCows += CalcCows(str(userInput)[n], mainNumber)
+    # keep a dictionary of digits and indexes where a bull was found
+    dictBullIndex = {}
 
-	return numberBulls, numberCows
+    for n in range(len(str(userInput))):
+        # if the position of digit in the input matches with the mainNumber then add 1 to numberBulls
+        if(str(mainNumber)[n] == str(userInput)[n]):
+            numberBulls += 1
 
-def CalcCows(userInputChar, mainNumber):
-	# this would return 1 if the digit is present in the actual number
+            # append the index to the list so that we know which places were exactly matched
+            dictBullIndex[int(mainNumber[n])] = dictBullIndex.get(int(mainNumber[n]), []) + [n]
+        else:
+            # if the digits at the corresponding position don't match, check if the digit is present at any other position
+            numberCows += CalcCows(str(userInput)[n], mainNumber, dictBullIndex)
 
-	if(str(mainNumber).find(userInputChar) >= 0):
-		return 1
+    return numberBulls, numberCows
 
-	return 0
+def CalcCows(userInputChar, mainNumber, dictBullIndex):
+    # find the list of index(s) of the userInputChar in the mainNumber
+    # if index != -1 and the index is not in dictBullIndex, then return 1
+    # else return 0
+
+    listIndex = []
+    n = 0
+    isCowFound = False
+
+    for characters in str(mainNumber):
+        if(characters == userInputChar):
+            listIndex.append(n)
+            isCowFound = True
+        n += 1
+
+    # get the list of indexes from dictBullIndex corresponding to the userInputChar, if it exists in the list
+    listBullIndex = dictBullIndex.get(int(userInputChar), "None")
+
+    if(isCowFound):
+        if(type(listBullIndex) == str):
+            return 1
+        elif(type(listBullIndex) == list):
+            for index in listBullIndex:
+                if(index in listIndex):
+                    listIndex.remove(index)
+
+            # if the list is empty then return 0. This means that the input had repeated digits and the main number had only 1 occurance of the digit
+            if(len(listIndex) == 0):
+                return 0
+            else:
+                return 1
+    else:
+        return 0
 
 
 def main():
-	numberDigits = 3 # number of digits
-	numberTries = 0
-	
-	# check user input
-	if(len(sys.argv) == 1):
-		StartGame(numberDigits)
-	elif(len(sys.argv) == 2):
-		# check if the 2nd input is actually an integer
-		try:
-			userLengthInput = int(sys.argv[1])
-			if(userLengthInput <= 0):
-				raise ValueError
-			else:
-				numberDigits = userLengthInput
-				StartGame(numberDigits)
-			return True
-		except ValueError:
-			print("\n*********************************************")
-			print("Invalid User Input: " + str(sys.argv[1]) + ". Please Enter A Valid Positive Integer")
-			return False
+    numberDigits = 3 # number of digits
+    numberTries = 0
+    
+    # check user input
+    if(len(sys.argv) == 1):
+        StartGame(numberDigits)
+    elif(len(sys.argv) == 2):
+        # check if the 2nd input is actually an integer
+        try:
+            userLengthInput = int(sys.argv[1])
+            if(userLengthInput <= 0):
+                raise ValueError
+            else:
+                numberDigits = userLengthInput
+                StartGame(numberDigits)
+            return True
+        except ValueError:
+            print("Invalid user input: " + str(sys.argv[1]) + ". Please enter a valid positive integer")
+            return False
 
 if __name__ == '__main__':
-	main()
+    main()
